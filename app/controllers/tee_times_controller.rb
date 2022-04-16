d4ffff5cc3dcb23717e532b1a14d61fe34cf0d97
April Skrine <april.skrine@gmail.com>
2022-04-16 14:54:24 -0500

    Update

--- app/controllers/tee_times_controller.rb
+++ app/controllers/tee_times_controller.rb
@@ -9,6 +9,16 @@ class TeeTimesController < ApplicationController
         render json: new_tee_time, status: :created
     end
 
+    def update
+        tee_time = TeeTime.find(params[:id])
+        if tee_time
+            tee_time.update(tee_time_params)
+            render json: tee_time
+        else
+            render json: { error: "Tee time not found" }, status: :not_found
+        end
+    end
+
     private
 
     def tee_time_params
--- app/controllers/user_tee_times_controller.rb
+++ app/controllers/user_tee_times_controller.rb
@@ -1,2 +1,12 @@
 class UserTeeTimesController < ApplicationController
+    def create
+        user_tee_time = UserTeeTime.create!(tee_time_params)
+        render json: user_tee_time, status: :created
+    end
+
+    private
+    
+    def tee_time_params
+        params.permit(:user_id, :tee_time_id)
+    end
 end
